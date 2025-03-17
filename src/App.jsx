f2b06cb30e7f219e4d317594b1071bd08fdc0dc3
toai-nguyen <maxngudan@gmail.com>
2025-03-17 17:00:24 +0700

    add initial server setup, integrate Bootstrap, and create Calculator component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import './css/App.css';
+import "./css/App.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Calculator from "./Pages/Calculator";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Calculator />} />
+          {/* <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Calculator.jsx
@@ -0,0 +1,11 @@
+import Result from "../Components/Result"
+import KeyBoard from "../Components/KeyBoard"
+
+export default function Calculator() {
+    return (
+        <div>
+            <Result />
+            <KeyBoard />
+        </div>
+    )
+}
